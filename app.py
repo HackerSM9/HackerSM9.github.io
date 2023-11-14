44c2559abeaf35f66f8f2aeef03361efbcab96e8
HackerSM9 <sm9hacking@gmail.com>
2023-11-14 11:31:48 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("C:\\Users\\Acer One\\GitHub\\HackerSM9.github.io\\templates\\index.html")
+    return render_template("sindex.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1', port=5000)
